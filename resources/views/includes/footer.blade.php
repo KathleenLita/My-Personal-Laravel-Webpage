81b8ac58c5f08fbe3025cb3a4b80206b64e75f65
Kathleen Lita <101811785+KathleenLita@users.noreply.github.com>
2022-06-16 22:20:52 -0700

    UPDATE

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,1 @@
+<div id="copyright text-right" style="text-align:center"><br><br>© Copyright <b>2022</b> Kathleen V. Lita </div>
---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,13 @@
+<div class="navbar">
+    <div class="navbar-inner">
+        <a id="logo" style="display:none">Kathleen Lita</a>
+        <ul class="nav">
+        <li><a href="#First">Title</a></li>
+        <li><a href="#Second">CS202 Favorite Lesson</a></li>
+        <li><a href="#Third">Laravel Topic</a></li>
+        <li><a href="#Fourth">About Me</a></li>
+        <li><a href="#Fifth">Six Years From Now</a></li>
+        <li><a href="#Sixth">Contact</a></li>
+        </ul>
+    </div>
+</div>
